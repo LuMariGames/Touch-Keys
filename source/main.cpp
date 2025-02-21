9bdb6f30edfd1226e05a468d9a999880a798f483
LuMariGames <kai.hono426@gmail.com>
2025-02-21 09:51:24 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -55,6 +55,42 @@ int main() {
 			draw_select_text(120, 100, "Now Loading...");
 			C3D_FrameEnd(0);
 			load_file_main();
+			scene = 1;
+			break;
+
+		case 1:	//選曲画面
+
+			if (cnt == 0) {
+				select_ini();
+			}
+
+			disp_file_list();
+			get_SelectedId(&SelectedSong, &course);
+
+			//下画面
+			C2D_TargetClear(bottom, C2D_Color32(0x42, 0x42, 0x42, 0xFF));
+			C3D_FrameDrawOn(bottom);
+			C2D_SceneTarget(bottom);
+
+			if (key & KEY_UP)		update_cursor(KEY_UP);
+			if (key & KEY_DOWN)		update_cursor(KEY_DOWN);
+			if (key & KEY_RIGHT)		update_cursor(KEY_RIGHT);
+			if (key & KEY_LEFT)		update_cursor(KEY_LEFT);
+			if (key & KEY_A)		update_cursor(KEY_A);
+			if (key & KEY_B)		update_cursor(KEY_B);
+
+			if (get_isGameStart() == true) {
+				scene_state = 2;
+				cnt = -1;
+			}
+			isPause = false;
+			if (key & KEY_START) isExit = true;
+			break;
+
+		case 2:	//ロード画面
+
+			
+		}
 
 		// 下画面に描画（必要に応じて描画）
 		C2D_TargetClear(bot, C2D_Color32(0, 0, 0, 1));
