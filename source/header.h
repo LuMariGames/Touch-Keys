a4b11cdb26cac93f2ba2accf56e17993960fbdb7
LuMariGames <kai.hono426@gmail.com>
2025-02-23 15:45:05 +0900

    Create header.h

---
+++ source/header.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <3ds.h>
+#include <citro2d.h>
+#include <limits.h>
+#include <math.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#define VERSION "0.1.0"
+#define DEFAULT_DIR "sdmc:/tkjfiles/"
+
+#define DEFAULT_JUDGE_RANGE_PERFECT	0.033
+#define DEFAULT_JUDGE_RANGE_NICE	0.067
+#define DEFAULT_JUDGE_RANGE_BAD		0.100
+
+#define TOP_WIDTH 400
+#define TOP_HEIGHT 240
+#define BOTTOM_WIDTH 320
+#define BOTTOM_HEIGHT 240
+#define NOTES_MEASURE_MAX 128
+#define MEASURE_MAX 2048
+#define NOTES_MAX 512
+#define JUDGE_Y 200
+
+#define LIST_MAX 4096
+#define DEFAULT_BUFFER_SIZE 2048
+
+enum COURSE_KND {
+
+	COURSE_EASY = 0,
+	COURSE_NORMAL,
+	COURSE_HARD,
+	COURSE_CRAZY,
+};
