f50089e2120a24dca8843bd4f8c6ecb7e06553c8
LuMariGames <kai.hono426@gmail.com>
2025-02-24 20:30:30 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -56,7 +56,7 @@ int main() {
 		for (int i = 0; i < NotesCount; ++i) {
 			if (ctoi(tkj_notes[MeasureCount][i]) != -1) {
 				Notes[i + MinNotesCnt].flag = true;
-				Notes[i + MinNotesCnt].num = ctoi(tkj_notes[MeasureCount][i];
+				Notes[i + MinNotesCnt].num = ctoi(tkj_notes[MeasureCount][i]);
 				Notes[i + MinNotesCnt].judge_time = (1.222 + OFFSET) + (240.0 / BPM * (MeasureCount - Startcnt)) + (240.0 / BPM * i / NotesCount);
 				++MaxNotesCnt;
 			}
