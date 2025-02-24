b95c282af7501f02e0394fa06fcb301724107eaa
LuMariGames <kai.hono426@gmail.com>
2025-02-24 19:29:12 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -14,7 +14,7 @@ const char* texturePath = "sdmc:/3ds/touch/image.t3x";
 char buffer[BUFFER_SIZE];
 char tkj_notes[MEASURE_MAX][NOTES_MEASURE_MAX];	//ノーツ情報
 int scene = 0,timecnt = 0,judgetmpcnt = 0,NotesSpeed = 200,touchid = -1,judgeid = -1,tkj_cnt = 0,
-NotesCount = 0,MinNotesCnt = 0,MaxNotesCnt = 0,Startcnt = 0,MeasureCount = 0,Score = 0,knd = -1;
+NotesCount = 0,MinNotesCnt = 0,MaxNotesCnt = 0,Startcnt = 0,MeasureCount = 0,Score = 0;
 double BPM = 120.0,OFFSET = 0,OffTime = 0,NowTime = 0;
 bool isExit = false,isPlayMain = false,isAuto = false;
 
@@ -54,10 +54,9 @@ int main() {
 		MaxNotesCnt = 0;
 		while (tkj_notes[MeasureCount][NotesCount] != ',' && tkj_notes[MeasureCount][NotesCount] != '\n') ++NotesCount;
 		for (int i = 0; i < NotesCount; ++i) {
-			knd = ctoi(tkj_notes[MeasureCount][i]);
-			if (knd != -1) {
+			if (ctoi(tkj_notes[MeasureCount][i]) != -1) {
 				Notes[i + MinNotesCnt].flag = true;
-				Notes[i + MinNotesCnt].num = knd;
+				Notes[i + MinNotesCnt].num = ctoi(tkj_notes[MeasureCount][i];
 				Notes[i + MinNotesCnt].judge_time = (1.222 + OFFSET) + (240.0 / BPM * (MeasureCount - Startcnt)) + (240.0 / BPM * i / NotesCount);
 				++MaxNotesCnt;
 			}
@@ -314,10 +313,9 @@ void Reset() {
 		NotesCount = 0;
 		MaxNotesCnt = 0;
 		for (int i = 0; i < NotesCount; ++i) {
-			knd = ctoi(tkj_notes[MeasureCount][i]);
-			if (knd != -1) {
+			if (ctoi(tkj_notes[MeasureCount][i]) != -1) {
 				Notes[i + MinNotesCnt].flag = true;
-				Notes[i + MinNotesCnt].num = knd;
+				Notes[i + MinNotesCnt].num = ctoi(tkj_notes[MeasureCount][i]);
 				Notes[i + MinNotesCnt].judge_time = (1.222 + OFFSET) + (240.0 / BPM * (MeasureCount - Startcnt)) + (240.0 / BPM * i / NotesCount);
 				++MaxNotesCnt;
 			}
