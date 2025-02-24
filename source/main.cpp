3ab6a736c32252457f0d4f176a8d387f04a18c59
LuMariGames <kai.hono426@gmail.com>
2025-02-24 18:26:04 +0900

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -54,9 +54,9 @@ int main() {
 		MaxNotesCnt = 0;
 		while (tkj_notes[MeasureCount][NotesCount] != ',' && tkj_notes[MeasureCount][NotesCount] != '\n') ++NotesCount;
 		for (int i = 0; i < NotesCount; ++i) {
-			if (tkj_notes[MeasureCount][NotesCount] != '0') {
+			if (ctoi(tkj_notes[MeasureCount][i]) != -1) {
 				Notes[i + MinNotesCnt].flag = true;
-				Notes[i + MinNotesCnt].num = ctoi(tkj_notes[MeasureCount][i]) - 1;
+				Notes[i + MinNotesCnt].num = ctoi(tkj_notes[MeasureCount][i]);
 				Notes[i + MinNotesCnt].judge_time = (1.222 + OFFSET) + (240.0 / BPM * (MeasureCount - Startcnt)) + (240.0 / BPM * i / NotesCount);
 				++MaxNotesCnt;
 			}
@@ -271,17 +271,17 @@ void tkjload() {
 int ctoi(char c) {
 
 	switch (c) {
-	case '0': return 0;
-	case '1': return 1;
-	case '2': return 2;
-	case '3': return 3;
-	case '4': return 4;
-	case '5': return 5;
-	case '6': return 6;
-	case '7': return 7;
-	case '8': return 8;
-	case '9': return 9; 
-	default: return 0;
+	case '0': return -1;
+	case '1': return 0;
+	case '2': return 1;
+	case '3': return 2;
+	case '4': return 3;
+	case '5': return 4;
+	case '6': return 5;
+	case '7': return 6;
+	case '8': return 7;
+	case '9': return 8;
+	default: return -1;
 	}
 }
 
@@ -313,9 +313,9 @@ void Reset() {
 		MaxNotesCnt = 0;
 		while (tkj_notes[MeasureCount][NotesCount] != ',' && tkj_notes[MeasureCount][NotesCount] != '\n') ++NotesCount;
 		for (int i = 0; i < NotesCount; ++i) {
-			if (tkj_notes[MeasureCount][i] != '0') {
+			if (ctoi(tkj_notes[MeasureCount][i]) != -1) {
 				Notes[i + MinNotesCnt].flag = true;
-				Notes[i + MinNotesCnt].num = ctoi(tkj_notes[MeasureCount][i]) - 1;
+				Notes[i + MinNotesCnt].num = ctoi(tkj_notes[MeasureCount][i]);
 				Notes[i + MinNotesCnt].judge_time = (1.222 + OFFSET) + (240.0 / BPM * (MeasureCount - Startcnt)) + (240.0 / BPM * i / NotesCount);
 				++MaxNotesCnt;
 			}
