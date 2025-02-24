34a7749292ba6aedb9d154fe7d57a05d4f3cda41
LuMariGames <kai.hono426@gmail.com>
2025-02-24 21:03:26 +0900

    Update main.h

--- source/main.h
+++ source/main.h
@@ -3,8 +3,8 @@
 
 typedef struct {
 	int num, notes_max, knd, roll_id, text_id;
-	double judge_time = 0, bpm, scroll, x_ini;
-	bool flag = false, isThrough;
+	double judge_time, bpm, scroll, x_ini;
+	bool flag, isThrough;
 	float y;
 
 } NOTES_T;
