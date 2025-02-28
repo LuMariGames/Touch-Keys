6c5f95bb21d92529acafe6dd9879fbe9bb46ca38
LuMariGames <kai.hono426@gmail.com>
2025-03-01 08:48:01 +0900

    Update main.h

--- source/main.h
+++ source/main.h
@@ -2,7 +2,6 @@
 #include <climits>
 
 typedef struct {
-
 	int level[4],x,y,tmp;
 	char title[256],path[256],tkj[256],wave[256];
 	bool course[4],course_exist[4];
@@ -13,8 +12,12 @@ typedef struct {
 	double judge_time = INT_MAX;
 	bool flag = false;
 	float y,bpm,scroll = 1.0f;
-
 } NOTES_T;
+
+typedef struct {
+	int measure,knd;
+	float num;
+} CMD_T;
 
 char *get_buffer();
 void draw_text(float x, float y, const char *text, float r, float g, float b);
