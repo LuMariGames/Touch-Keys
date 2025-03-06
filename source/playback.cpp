7a1691910782f858b6594c2057ce20e2d8d8ec6b
LuMariGames <kai.hono426@gmail.com>
2025-03-06 16:44:07 +0900

    Update playback.cpp

--- source/playback.cpp
+++ source/playback.cpp
@@ -77,7 +77,7 @@ void playFile(void* infoIn){
 
 	memset(waveBuf, 0, sizeof(waveBuf));
 
-	while (*info->isPlay == false) svcSleepThread(100000);
+	while (*info->isPlay == false) svcSleepThread(20000);
 
 	waveBuf[0].nsamples = (*decoder.decode)(&buffer1[0]) / (*decoder.channels)();
 	waveBuf[0].data_vaddr = &buffer1[0];
