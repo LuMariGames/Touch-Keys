de6b1f208268597dcec2daa1e1bc6a299f8492d4
LuMariGames <kai.hono426@gmail.com>
2025-05-09 21:54:25 +0900

    Update header.h

--- source/header.h
+++ source/header.h
@@ -11,7 +11,7 @@
 #include <unistd.h>
 #include <ctype.h>
 
-#define VERSION "2.0.3"
+#define VERSION "2.1.0"
 #define DEFAULT_DIR "sdmc:/tkjfiles/"
 #define SETTING_FILE "sdmc:/TouchKeys_config.json"
 
